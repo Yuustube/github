be24cc575d7efe561048c61c0ce9531e1ced107b
unknown <yuustube>
2019-03-27 15:51:28 +0900

    Initial commit

---
+++ 웹클라이언트컴퓨팅/lab4/lab4/movie_crawling_lite.js
@@ -0,0 +1,20 @@
+﻿ 
+const cheerio = require('cheerio');
+const request = require('request');
+const iconv = require('iconv-lite');
+ 
+ 
+let url = "http://movie.naver.com/movie/sdb/rank/rmovie.nhn";
+ 
+request({url, encoding: null}, function(error, response, body){
+    let htmlDoc = iconv.decode(body, 'CP949');
+//    let htmlDoc = body;
+    let resultArr = [];
+ 
+    const $ = cheerio.load(htmlDoc);
+    let colArr = $(".tit3 a")
+    for(let i = 0; i < colArr.length; i++)
+        resultArr.push(colArr[i].attribs.title)
+ 
+    console.log(resultArr)
+});

45d52eb0fe34dbf08c6da9e530af8c8ace59b25c
unknown <yuustube>
2019-04-09 19:46:49 +0900

    Initial commit

--- 웹클라이언트컴퓨팅/lab4/lab4/movieServer.js
+++ 웹클라이언트컴퓨팅/lab4/lab4/movieServer.js
@@ -1,3 +1,43 @@
+
+const cheerio = require('cheerio');
+const request = require('request');
+const iconv = require('iconv-lite');
+let titles = new Array();
+let images = new Array();
+let genres = new Array();
+
+let url = "https://movie.naver.com/movie/running/current.nhn";
+
+request({url, encoding: null}, function(error, response, body){
+    let htmlDoc = iconv.decode(body, 'UTF-8');
+//    let htmlDoc = body;
+    let resultArr = [];
+
+    const $ = cheerio.load(htmlDoc);
+    let colArr = $("dl.lst_dsc")
+    var names;
+    for(let i = 0; i < colArr.length; i++) {
+        let title = $(`div.lst_wrap > ul > li:nth-child(${i+1}) > dl > dt > a`).text();
+        titles[i] = title;
+        let src = $(`div.lst_wrap > ul > li:nth-child(${i+1}) > div > a > img`).attr('src');
+        let image = $(`div.lst_wrap > ul > li:nth-child(${i+1}) > div > a`).html();
+        images[i] = image;
+        let genre = '';
+        $(`div.lst_wrap > ul > li:nth-child(${i+1}) > dl > dd:nth-child(3) > dl > dd:nth-child(2) > span.link_txt > a`).each(function(index, obj) {
+            genre += $(this).text() +', '; });
+        genre = genre.substring(0, genre.length-2);
+        genres[i] = genre;
+        //let reserve = $(`div.lst_wrap > ul > li:nth-child(${i+1}); //> dl > dd:nth-child(${1}) > dd:nth-child(${1}) > dl:nth-child(${2}) > dd > div > span:nth-child(${1})`).text();
+        //console.log(reserve)
+
+      }
+
+      //console.log(title)
+
+    //console.log(resultArr)
+});
+
+
 // 모듈을 추출합니다.
 const express = require('express');
 // 서버를 생성합니다.
@@ -5,47 +45,34 @@ const app = express();
 // request 이벤트 리스너를 설정합니다.
 app.use((request, response) => {
 // 데이터소스: https://movie.naver.com/movie/running/current.nhn
+
     let output = ""
-    
+
     output += "<style>";
     output += ".row { width: 990px; text-align: left; margin-top: -10px; margin-bottom: -10px; }";
     output += ".block {  width: 330px; vertical-align: middle; display: inline-block; line-height: 7px; }​";
     output += ".block h5 { margin-bottom: 1.71429rem; margin-top: 1.71429rem; }";
     output += "</style>";
 
-    output += "<div class='row'>";
-    output += "<div class='block'>";
-    output += "<h2>1</h2>";
-    output += "</div>";
-    output += "<div class='block'>";
-    output += '<img src="https://movie-phinf.pstatic.net/20190306_280/1551849045570X4iac_JPEG/movie_image.jpg?type=m99_141_2" alt="돈">';
-    output += "</div>";
-    output += "<div class='block'>";
-    output += "<h5>제목: 돈</h5>";
-    output += "<h5>예매율: 53.29%</h5>";
-    output += "<h5>개요: 범죄</h5>";
-    output += "<h5>감독: 박누리</h5>";
-    output += "<h5>출연: 류준열, 유지태, 조우진</h5>";
-    output += "</div>";
-    output += "</div>";
-    output += "<hr/>";
 
-    output += "<div class='row'>";
-    output += "<div class='block'>";
-    output += "<h2>2</h2>";
-    output += "</div>";
-    output += "<div class='block'>";
-    output += '<img src="https://movie-phinf.pstatic.net/20190225_72/1551069530582h2huX_JPEG/movie_image.jpg?type=m99_141_2" alt="캡틴 마블">';
-    output += "</div>";
-    output += "<div class='block'>";
-    output += "<h5>제목: 캡틴 마블</h5>";
-    output += "<h5>예매율: 24.86%</h5>";
-    output += "<h5>개요: 액션, 모험</h5>";
-    output += "<h5>감독: 애너 보든, 라이언 플렉</h5>";
-    output += "<h5>출연: 브리 라슨, 사무엘 L. 잭슨, 벤 멘델슨, 주드 로</h5>";
-    output += "</div>";
-    output += "</div>";
-    output += "<hr/>";
+    for(let i = 0; i < 50; i++){
+      output += "<div class='row'>";
+      output += "<div class='block'>";
+      output += "<h2>"+ (i+1) +"</h2>";
+      output += "</div>";
+      output += "<div class='block'>";
+      output += images[i];
+      output += "</div>";
+      output += "<div class='block'>";
+      output += "<h5>제목: " + titles[i]  + " </h5>";
+      output += "<h5>예매율: 53.29%</h5>";
+      output += "<h5>개요: "+ genres[i] +"</h5>";
+      output += "<h5>감독: 박누리</h5>";
+      output += "<h5>출연: 류준열, 유지태, 조우진</h5>";
+      output += "</div>";
+      output += "</div>";
+      output += "<hr/>";
+    }
 
     response.send(output);
 });
